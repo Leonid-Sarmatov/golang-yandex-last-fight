e17d5667b135810caef182a6e5f9a9df02e0d8f3
leonid <chemestru@gmail.com>
2024-03-26 22:46:39 +0300

    Creating packages and files

---
+++ frontend_server/internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	EnvMode          string `yaml:"environment_mode" env-default:"local"`
+	HTTPServerConfig `yaml:"http_server"`
+}
+
+type HTTPServerConfig struct {
+	Address           string        `yaml:"address" env-default:"localhost:8081"`
+	RequestTimeout    time.Duration `yaml:"request_timeout" env-default:"4s"`
+	ConnectionTimeout time.Duration `yaml:"connection_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	// Проверяем задан ли путь до конфиг-файла
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is empty")
+	}
+
+	// Проверяем существует ли конфиг-файл
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file is not exist: %v", configPath)
+	}
+
+	// Считываем конфиги
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("Reading config was failed: %v", err)
+	}
+	return &cfg
+}
---
+++ frontend_server/internal/site_handler/site_handler.go
@@ -0,0 +1,5 @@
+package site_handler
+
+import (
+	
+)
